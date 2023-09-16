8eb94f6b6ffcc5bf34d9f7261876adccf0324fad
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-17 02:52:23 +0530

    adding diff. files and making button responsive

---
+++ lib/pages/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'page2.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        shadowColor: Color.fromARGB(255, 80, 27, 215),
+        title: const Text("Yoga"),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
+            ),
+            child: Column(children: [
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 20),
+                alignment: Alignment.topCenter,
+                child: ClipRRect(
+                  borderRadius: BorderRadiusDirectional.horizontal(),
+                  child: Image.asset('assets/image1.jpg'),
+                ),
+              ),
+              Expanded(
+                child: Column(children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 10),
+                    child: Text(
+                      'Yoga classes',
+                      style: TextStyle(
+                          fontSize: 38,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 10, bottom: 50),
+                        child: const Text(
+                          ''' Meditation is the key to
+        Productivity, Happiness & Longitivity.''',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ]),
+              ),
+              Expanded(
+                  child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
+                        child: Text('Next'),
+                        onPressed: () {
+                          print("Called - 1");
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Page2()),
+                          );
+                        }),
+                  )
+                ],
+              ))
+            ]),
+          ),
+        ],
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
---
+++ lib/pages/page3.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Page3 extends StatelessWidget {
+  const Page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(child: Text("Hey")),
+    );
+  }
+}
