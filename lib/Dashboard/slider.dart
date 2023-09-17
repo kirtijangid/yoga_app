c3fd87c5d2a59c5ef35441599924ef6261907eb6
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-17 23:02:31 +0530

    init branch files from main

---
+++ lib/Dashboard/slider.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
+  @override
+  State<ImageSlider> createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  List imageList = [
+    {"id": 1, "image_path": 'assets/beginner2.jpg'},
+    {"id": 1, "image_path": 'assets/intermediate.jpg'},
+    {"id": 1, "image_path": 'assets/advanced2.png'}
+  ];
+  final CarouselController carouselController = CarouselController();
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Yoga Workout'),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              InkWell(
+                onTap: () {
+                  print(currentIndex);
+                },
+                child: CarouselSlider(
+                  items: imageList
+                      .map(
+                        (item) => Image.asset(
+                          item['image_path'],
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      )
+                      .toList(),
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                    scrollPhysics: const BouncingScrollPhysics(),
+                    autoPlay: true,
+                    aspectRatio: 2,
+                    viewportFraction: 1,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
