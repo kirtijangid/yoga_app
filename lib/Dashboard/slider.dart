fc558a47390531a4368e1c6defc339e4e8f0a7f1
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-18 14:50:03 +0530

    mutiple types of sliders

--- lib/Dashboard/slider.dart
+++ lib/Dashboard/slider.dart
@@ -8,11 +8,19 @@ class ImageSlider extends StatefulWidget {
   State<ImageSlider> createState() => _ImageSliderState();
 }
 
-class _ImageSliderState extends State<ImageSlider> {
-  List imageList = [
-    {"id": 1, "image_path": 'assets/beginner2.jpg'},
-    {"id": 1, "image_path": 'assets/intermediate.jpg'},
-    {"id": 1, "image_path": 'assets/advanced2.png'}
+class _ImageSliderState extends State<ImageSlider>
+    with TickerProviderStateMixin {
+  List<String> assets = [
+    'assets/beginner2.jpg',
+    'assets/intermediate.jpg',
+    'assets/advanced2.png'
+  ];
+  final Color = [
+    Colors.red,
+    Colors.amber,
+    Colors.teal,
+    Colors.blueGrey,
+    Colors.blue
   ];
   final CarouselController carouselController = CarouselController();
   int currentIndex = 0;
@@ -24,41 +32,100 @@ class _ImageSliderState extends State<ImageSlider> {
         centerTitle: true,
         title: Text('Yoga Workout'),
       ),
-      body: Column(
-        children: [
-          Stack(
+      body: Container(
+        margin: EdgeInsets.only(top: 50),
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              InkWell(
-                onTap: () {
-                  print(currentIndex);
-                },
-                child: CarouselSlider(
-                  items: imageList
-                      .map(
-                        (item) => Image.asset(
-                          item['image_path'],
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                        ),
-                      )
-                      .toList(),
-                  carouselController: carouselController,
-                  options: CarouselOptions(
-                    scrollPhysics: const BouncingScrollPhysics(),
-                    autoPlay: true,
-                    aspectRatio: 2,
-                    viewportFraction: 1,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                  ),
+              // SizedBox(
+              //   height: 300,
+              //   width: MediaQuery.of(context).size.width,
+              //   child: PageView.builder(
+              //     itemCount: assets.length,
+              //     padEnds: false,
+              //     pageSnapping: false,
+              //     physics: BouncingScrollPhysics(),
+              //     reverse: true,
+              //     controller:
+              //         PageController(initialPage: 3, viewportFraction: 1),
+              //     itemBuilder: (context, index) {
+              //       return Container(
+              //         margin: EdgeInsets.all(8),
+              //         clipBehavior: Clip.antiAlias,
+              //         decoration: BoxDecoration(
+              //           //color: Color[index],
+              //           borderRadius: BorderRadius.circular(25),
+              //         ),
+              //         child: Image.asset(
+              //           assets[index],
+              //           fit: BoxFit.cover,
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // ),
+              SizedBox(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: PageView.builder(
+                  itemCount: assets.length,
+                  physics: BouncingScrollPhysics(),
+                  controller:
+                      PageController(initialPage: 0, viewportFraction: 1),
+                  onPageChanged: (value) {
+                    currentIndex = value;
+                    setState(() {});
+                  },
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                          //color: Color[index],
+                          borderRadius: BorderRadius.circular(25)),
+                      child: Image.asset(
+                        assets[index],
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
                 ),
               ),
+              TabPageSelector(
+                controller: TabController(
+                    length: assets.length,
+                    initialIndex: currentIndex,
+                    vsync: this),
+                selectedColor: Colors.red,
+                color: Colors.grey,
+                borderStyle: BorderStyle.none,
+              ),
+              SizedBox(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                child: PageView.builder(
+                  physics: BouncingScrollPhysics(),
+                  controller:
+                      PageController(initialPage: 3, viewportFraction: 0.7),
+                  itemBuilder: (context, index) {
+                    print(index % assets.length);
+                    return Container(
+                      margin: EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                          //color: Color[index]
+                          borderRadius: BorderRadius.circular(25)),
+                      child: Image.asset(
+                        assets[index % assets.length],
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
