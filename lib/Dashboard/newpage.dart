4baefbf3407e6138ae63b5620b0014411f409097
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-19 22:38:37 +0530

    adding images to the file as list

--- lib/Dashboard/newpage.dart
+++ lib/Dashboard/newpage.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-class SeasonalYoga extends StatelessWidget {
+class SeasonalYoga extends StatefulWidget {
   const SeasonalYoga({Key? key}) : super(key: key);
 
+  @override
+  State<SeasonalYoga> createState() => _SeasonalYogaState();
+}
+
+class _SeasonalYogaState extends State<SeasonalYoga> {
+  List<String> assets = [
+    'assets/winteryoga1.jpg',
+    'assets/fallyoga1.jpg',
+    'assets/summerseason1.jpg'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
