e20c4c806ed9321024d54d84e41d23f2a720f6fb
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-20 04:14:14 +0530

    adding image to yaml file

--- lib/Dashboard/activity.dart
+++ lib/Dashboard/activity.dart
@@ -19,6 +19,33 @@ class _ActivityState extends State<Activity> {
           style: TextStyle(color: Colors.black),
         ),
       ),
+      body: Container(
+        margin: EdgeInsets.only(top: 10),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 20, bottom: 20),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Starts | today',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    alignment: Alignment.bottomLeft,
+                  ),
+                  
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
