594639c26c936e5802b01bcc94b7cd9bd079eac2
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-19 23:47:40 +0530

    adding container for the text

--- lib/Dashboard/newpage.dart
+++ lib/Dashboard/newpage.dart
@@ -123,6 +123,7 @@ class _SeasonalYogaState extends State<SeasonalYoga> {
                   },
                 ),
               ),
+              Container(child: Text('Body Fitness'),)
             ],
           ),
         ),
