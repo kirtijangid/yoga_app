3af91f19505486d43086d70e4c774fb03f385dd4
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-20 04:29:53 +0530

    creating navigation bar

--- lib/Dashboard/activity.dart
+++ lib/Dashboard/activity.dart
@@ -48,6 +48,48 @@ class _ActivityState extends State<Activity> {
           ]),
         ),
       ),
+      resizeToAvoidBottomInset: false,
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 2,
+          selectedItemColor: Colors.red,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home",
+              //backgroundColor: Colors.blueGrey,
+              icon: Icon(Icons.home, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              label: "Seasonal Yoga",
+              // backgroundColor: Colors.blueGrey,
+              icon: Icon(Icons.search, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              label: "Activity",
+              // backgroundColor: Colors.blueGrey,
+              icon: Icon(Icons.bar_chart, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              // backgroundColor: Colors.blueGrey,
+              icon: Icon(Icons.settings, color: Colors.black),
+            ),
+          ],
+          onTap: (int indexOfItem) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Settings()),
+            );
+          }),
     );
+  }
+}
+
+class Settings extends StatelessWidget{
+  const Settings();
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
