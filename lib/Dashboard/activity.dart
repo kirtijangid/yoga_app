02cd22779004a5c151f6ee805bcfde7829d325fd
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-20 23:44:25 +0530

    adding image

--- lib/Dashboard/activity.dart
+++ lib/Dashboard/activity.dart
@@ -330,6 +330,10 @@ class _ActivityState extends State<Activity> {
                       ],
                     ),
                   ),
+
+                  Container(
+                    
+                  )
                 ],
               ),
             ),
