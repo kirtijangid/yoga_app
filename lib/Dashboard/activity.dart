31881d0ec68f78f5ad7810ecc1bc97886568612c
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-20 15:58:54 +0530

    adding BMI chart

--- lib/Dashboard/activity.dart
+++ lib/Dashboard/activity.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Activity extends StatefulWidget {
@@ -8,6 +10,50 @@ class Activity extends StatefulWidget {
 }
 
 class _ActivityState extends State<Activity> {
+
+final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double? bmi;
+  String errorText = '';
+  String status = '';
+
+  void calculateBMI() {
+    final double? height = double.tryParse(_heightController.value.text);
+    final double? weight = double.tryParse(_weightController.value.text);
+
+    if (height == null || weight == null) {
+      setState(() {
+        errorText = "Please enter your Height and Weight";
+      });
+      return;
+    }
+
+    if (height <= 0 || weight <= 0) {
+      setState(() {
+        errorText = "Your Height and Weight must be positive numbers";
+      });
+      return;
+    }
+
+    setState(() {
+      bmi = weight / pow((height / 100), 2);
+      if (bmi! < 18.5) {
+        status = "Underweight";
+      } else if (bmi! > 18.5 && bmi! < 25) {
+        status = 'Normal weight';
+      } else if (bmi! > 25 && bmi! < 30) {
+        status = 'Pre-Obesity';
+      } else if (bmi! > 30 && bmi! < 35) {
+        status = 'Obesity class 1';
+      } else if (bmi! > 35 && bmi! < 40) {
+        status = 'Obesity class 2';
+      } else {
+        status = 'Obesity class 3';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,8 +87,15 @@ class _ActivityState extends State<Activity> {
                   ),
                   Container(
                     child: Image.asset('assets/kcal2.jpg'),
-                  )
-                ],
+                  ),
+
+                  Container(
+                    child: Image.asset('assets/BMI.png'),
+                  ),
+                  Container(
+                    child:
+                    Text('data')
+              ),],
               ),
             ),
           ]),
@@ -84,6 +137,8 @@ class _ActivityState extends State<Activity> {
     );
   }
 }
+
+
 
 class Settings extends StatelessWidget {
   const Settings();
