608778d031ea0f0c11d76aa7a7001ebc1e813bd5
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-21 00:05:35 +0530

    creating navigation bar

--- lib/Dashboard/settings.dart
+++ lib/Dashboard/settings.dart
@@ -10,6 +10,37 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        selectedItemColor: Colors.red,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            //backgroundColor: Colors.blueGrey,
+            icon: Icon(Icons.home, color: Colors.black),
+          ),
+          BottomNavigationBarItem(
+            label: "Seasonal Yoga",
+            // backgroundColor: Colors.blueGrey,
+            icon: Icon(Icons.search, color: Colors.black),
+          ),
+          BottomNavigationBarItem(
+            label: "Activity",
+            // backgroundColor: Colors.blueGrey,
+            icon: Icon(Icons.bar_chart, color: Colors.black),
+          ),
+          BottomNavigationBarItem(
+            label: "Settings",
+            // backgroundColor: Colors.blueGrey,
+            icon: Icon(Icons.settings, color: Colors.black),
+          ),
+        ],
+        onTap: (int indexOfItem) {
+          
+        },
+      ),
+    );
   }
 }
